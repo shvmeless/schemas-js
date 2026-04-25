b15656df5f31d4cad0f91b38b5970e8538d63b89
Luciano Aedo Puga <81434301+shvmeless@users.noreply.github.com>
2026-04-25 13:13:26 -0400

    feat: add the `RecordSchema` class

---
+++ src/schemas/RecordSchema.ts
@@ -0,0 +1,48 @@
+// IMPORTS
+import { GenericSchema } from '@/interfaces'
+import { ValidationError, ValidationErrorIndex } from '@/errors/ValidationError'
+
+// CLASS
+export class RecordSchema<T> implements GenericSchema<Record<string, T>> {
+
+  // PROPERTIES
+  private readonly _schema: GenericSchema<T>
+
+  // CONSTRUCTOR
+  private constructor(schema: GenericSchema<T>) {
+    this._schema = schema
+  }
+
+  // CONSTRUCTOR
+  public static create<T>(schema: GenericSchema<T>): RecordSchema<T> {
+    return new RecordSchema<T>(schema)
+  }
+
+  // METHOD
+  public validate(input: unknown): Record<string, T> {
+
+    if (typeof input !== 'object' || input === null || Array.isArray(input) || (Object.getPrototypeOf(input) !== Object.prototype && Object.getPrototypeOf(input) !== null)) {
+      throw new ValidationError(input, 'The value must be an object.')
+    }
+
+    const result: Record<string, T> = {}
+    const errors: ValidationErrorIndex = {}
+
+    for (const [key, value] of Object.entries(input)) {
+      try {
+        result[key] = this._schema.validate(value)
+      } catch (error) {
+        if (error instanceof ValidationError) errors[key] = error.index ?? error.message
+        else throw error
+      }
+    }
+
+    if (Object.keys(errors).length > 0) {
+      throw new ValidationError(input, 'At least one entry does not match the given schema.', errors)
+    }
+
+    return result
+
+  }
+
+}
